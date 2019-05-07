5139f6001c1820677677e649e1c3bec81772ccdb
Steven Mercatante <steven.mercatante@gmail.com>
2019-05-07 17:03:43 -0400

    clean up

--- test_pipeline.py
+++ test_pipeline.py
@@ -16,45 +16,6 @@ class JsonCoder(object):
     return json.loads(x)
 
 
-# class DataIngestion:
-#     """A helper class which contains the logic to translate the file into 
-#     a format BigQuery will accept."""
-
-#     def parse_method(self, string_input):
-#         """This method translates a single line of comma separated values to a 
-#         dictionary which can be loaded into BigQuery.
-        
-#         Args:
-#             string_input: A comma separated list of values in the form of 
-#             state_abbreviation,gender,year,name,count_of_babies,dataset_created_date
-#                 example string_input: KS,F,1923,Dorothy,654,11/28/2016
-        
-#         Returns:
-#             A dict mapping BigQuery column names as keys to the corresponding value
-#             parsed from string_input.  In this example, the data is not transformed, and 
-#             remains in the same format as the CSV.  There are no date format transformations. 
-#             example output:
-#               {'state': 'KS',
-#                'gender': 'F',
-#                'year': '1923', <- Keep the format of the "date" as a simple integer, with no transformations.
-#                'name': 'Dorothy',
-#                'number': '654',
-#                'created_date': '11/28/2016'
-#                }
-#          """
-#         # Strip out return characters and quote characters.
-#         values = re.split(",",
-#                           re.sub('\r\n', '', re.sub(u'"', '', string_input)))
-
-#         row = dict( zip(('state', 'gender', 'year', 'name', 'number',
-#                          'created_date'),
-#                 values))
-
-#         return row
-
-
-def parse_data(s):
-    return s
 
 def run(argv=None):
     """The main function which creates the pipeline and runs it."""
